91aa4e681c447c9685b8806e914d8a683d431714
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-09-13 22:17:49 -0400

    buttons ^^

--- src/demo.js
+++ src/demo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -12,11 +14,49 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const TimeSlotsPerDay = () => {
+  const classes = useStyles();
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      textAlign="left"
+      fontWeight="fontWeightRegular"
+      fontSize={16}
+    >
+      <Typography variant="h6" gutterBottom>
+        Select Time
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Quos blanditiis tenetur
+      </Typography>
+      <Box display="flex">
+        <Button variant="outlined" color="primary" className={classes.button}>
+          7:30am
+        </Button>
+        <Button variant="outlined" color="primary" className={classes.button}>
+          8:00am
+        </Button>
+        <Button variant="outlined" color="primary" className={classes.button}>
+          8:30am
+        </Button>
+        <Button variant="outlined" color="primary" className={classes.button}>
+          9:00am
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 export default function GroupedButtons() {
   // we have to extract the classes object from the object returned from usestyles();
   const classes = useStyles();
   return (
     <Grid container spacing={3}>
+      <Grid item>
+        <TimeSlotsPerDay />
+      </Grid>
       <Grid item>
         <ButtonGroup size="small" aria-label="small outlined button group">
           <Button>One</Button>
